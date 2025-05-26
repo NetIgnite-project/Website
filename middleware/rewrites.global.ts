db027cdfd5f34c0b040aef4e4aef66727c7bb8d7
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-05-26 21:39:15 +0200

    updated rewrites

--- middleware/rewrites.global.ts
+++ middleware/rewrites.global.ts
@@ -3,7 +3,7 @@ export default defineNuxtRouteMiddleware((to) => {
 
     if (to.path.endsWith('/') && to.path !== '/') {
         // Redirect to the path without the trailing slash  
-        return navigateTo(to.path.slice(0, 1), { replace: true });
+        return navigateTo(to.path.slice(0, -1), { replace: true });
     }
 
 });
